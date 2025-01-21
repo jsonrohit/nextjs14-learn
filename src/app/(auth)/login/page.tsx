d980a334de36b14a77bc969eacd0a9d61777735f
jsonrohit <rohit050996@gmail.com>
2025-01-21 12:44:21 +0530

    auth module fixes

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import style from './styles.module.css'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import { httpRequest } from '../../services/apiRequest'
 
 // export const metadata = {
 //     title: 'Login Page',
@@ -13,36 +14,28 @@ import { useRouter } from 'next/navigation'
 export default function Login() {
     const router = useRouter()
     const [state, setState] = useState({});
-    const [error, setError] = useState();
-
+    const [error, setError] = useState('');
 
     const onChange = (e) => {
         const value = e.target.value;
-        console.log(e.target.name, 'ddd');
         setState(prevState => ({
             ...prevState,
             [e.target.name]: value,
         }));
     }
 
-    useEffect(()=>
-    {
-console.log(error,'usefect errro');
-    },[error])
-
-    const submitHandler = async () => {  
-            const res = await fetch('api/login', {
-                body: JSON.stringify(state),
-                method: 'POST',
-            })
-            console.log(res,'gggggg');
+    const submitHandler = async () => {
+        const datas = await httpRequest('login', {
+            body: JSON.stringify(state),
+            method: 'POST',
+        })
+        const res = await datas.json();
         if (res.status == 200) {
             router.push('posts')
-        } else if(res.status == 400) {
-            console.log(await res.json(),'ffffffff');
+        } else if (res && res.status == 400) {
             setError(res.errors)
             //setError('Enter vaild email & password')
-        }else{
+        } else {
             setError('Enter vaild email & password')
         }
     }
@@ -65,7 +58,7 @@ console.log(error,'usefect errro');
                         <button onClick={submitHandler} className='btn btn-info me-2'>Login</button>
                     </div>
                     <div>
-                        <h6>Testing Login Credentials </h6>
+                        <h6>Test Login Credentials </h6>
                         <small>Email: test@gmail.com</small><br />
                         <small>Password: test@123</small>
                     </div>
--- src/app/services/apiRequest.js
+++ src/app/services/apiRequest.js
@@ -1,6 +1,6 @@
-const httpRequest = async (url, body = null) => {
+const httpRequest = async (url, body = {}) => {
     const API_URL = process.env.NEXT_PUBLIC_API_URL
-    return await fetch(`${API_URL}/${url}`, { body })
+    return await fetch(`${API_URL}/${url}`, body)
 }
 
 export { httpRequest };
